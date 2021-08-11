623267e2b7b75fbdf1f05f720fbbcc1a0e241542
ZFortis <zyfortis@gmail.com>
2021-08-11 16:39:05 +0800

    增加了通过Wecom推送的方式

--- package/send_message.py
+++ package/send_message.py
@@ -1,33 +1,44 @@
 import aiohttp
 import asyncio
+import json
+import requests
 
 
 class SessionAio:
-    def __init__(self, tg_token: str = '', tg_id: str = '', sc_token: str = '', loop=None):
+    def __init__(self, tg_token, tg_id, sc_token, et_id, app_id, app_se, loop=None):
         self.tg_token = tg_token
         self.tg_id = tg_id
         self.sc_token = sc_token
+        self.et_id = et_id
+        self.app_id = app_id
+        self.app_se = app_se
         self.loop = loop or asyncio.get_event_loop()
 
-    async def send(self, tg: bool = False, tg_text: str = '',
-                   sc: bool = False, sc_title: str = '', sc_desp: str = '',
-                   text: str = ''):
+
+    async def send(self, tg=False, tg_text='',
+                    sc=False, sc_title='', sc_desp='',
+                    we=False,
+                    text=''):
         task = self.loop.create_task(self.main(tg=tg, tg_text=tg_text,
                                                sc=sc, sc_title=sc_title, sc_desp=sc_desp,
+                                               we=we,
                                                text=text))
         await task
         print('result', task.result())
         return task.result()
 
-    async def main(self, tg: bool = False, tg_text: str = '',
-                   sc: bool = False, sc_title: str = '', sc_desp: str = '',
-                   text: str = ''):
+    async def main(self, tg=False, tg_text='',
+                   sc=False, sc_title='', sc_desp='',
+                   we=False,
+                   text=''):
         result = []
         run_detail = {}
         if tg is True:
             result.append(self.loop.create_task(self.tg_send(tg_text=tg_text, text=text)))
         if sc is True:
             result.append(self.loop.create_task(self.sc_send(sc_title=sc_title, sc_desp=sc_desp, text=text)))
+        if we is True:
+            result.append(self.loop.create_task(self.we_send(text=text)))
         for z in result:
             await z
         for y in result:
@@ -58,6 +69,33 @@ class SessionAio:
                 except:
                     pass
         return ['sc', False]
+
+    async def we_send(self, text: str = '') -> list:
+        wecom_touid = '@all'
+        get_token_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={self.et_id}&corpsecret={self.app_se}"
+        response = requests.get(get_token_url).content
+        access_token = json.loads(response).get('access_token')
+        if access_token and len(access_token) > 0:
+            send_msg_url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
+            data = {
+                "touser":wecom_touid,
+                "agentid":self.app_id,
+                "msgtype":"text",
+                "text":{
+                    "content":text
+                },
+                "duplicate_check_interval":600
+            }
+            print('ERROR1')
+            async with aiohttp.ClientSession() as session:
+                for a in [0, 1, 2]:
+                    try:
+                        await session.post(url=send_msg_url, data=json.dumps(data), timeout=3)
+                        return ['we', True]
+                    except:
+                        print('ERROR2')
+                    
+            return ['we', False]
 
 
 if __name__ == '__main__':
